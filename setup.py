8b3225836f03905968252617f306f94a50e894d5
toshayas <toshayas@cisco.com>
2019-12-31 00:52:18 +0900

    change version from string to fload

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="cfgrep",
-    version="1.0.0",
-    description="fgrep(c4grep, ccccgrep) is Cisco Config Context-aware Check grep",
+    version=1.0,
+    description="cfgrep is Cisco Config Context-aware Check grep",
     author="toshayas",
     author_email='toshayas@cisco.com',
     url='https://github.com/tk-hayashi/cfgrep',
